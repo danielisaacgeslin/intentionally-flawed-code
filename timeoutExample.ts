132c709e2656ab69ac113ef3d7eaac3e2e3911ca
Daniel Isaac Geslin <isaacgeslin@gmail.com>
2021-06-04 17:10:57 +0200

    typo

--- timeoutExample.ts
+++ timeoutExample.ts
@@ -1,11 +1,11 @@
 function first() {
   console.log(1);
   setTimeout(() => console.log(2), 2000);
-  console.log(2);
+  console.log(3);
 }
 
 function second() {
   console.log(1);
   setTimeout(() => console.log(2), 0);
-  console.log(2);
+  console.log(3);
 }
